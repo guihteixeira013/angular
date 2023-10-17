bf5043b5c6dfbda00fc4edafc7529536baaeb6f4
Guilherme <guilherme.teixeira013@protonmail.com>
2023-10-17 11:30:05 -0300

    creating new components

---
+++ my-first-angular/src/app/hello/hello.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+// @Component é um decorator
+@Component({  // transmitindo 1 objeto ( {} ) com metadados
+  // 3 índices : selector, templateUrl e StyleUrls
+
+    // qual a tag HTML que usaremos pra representar esse componente? 
+  selector: 'app-hello', // (<app-hello> </app-hello)
+
+    // qual o caminho que está o arquivo html contendo o conteúdo visual do componente?
+  templateUrl: './hello.component.html',
+
+    // quais os caminhos de arquivos para estilização
+  styleUrls: ['./hello.component.css']
+
+  // NOTA: Os arquivos de estilização do componente será utilizada apenas pelos arquivos .html do próprio componente.
+})
+
+export class HelloComponent {
+
+}
