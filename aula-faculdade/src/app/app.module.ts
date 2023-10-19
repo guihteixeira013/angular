bf9ea7530ce9f08b0657e043b6699691ee508bc9
Guilherme <guilherme.teixeira013@protonmail.com>
2023-10-19 10:20:49 -0300

    Criando módulo em angular e compartilhando componente

--- aula-faculdade/src/app/app.module.ts
+++ aula-faculdade/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header.component';
+import { ComponentModule } from './components/components.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    ComponentModule,
     AppRoutingModule
   ],
   providers: [],
