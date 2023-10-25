2f653c5a8c2302e1b5bd66b294dda9b93c822cf8
Guilherme <guilherme.teixeira013@protonmail.com>
2023-10-25 10:46:10 -0300

    Data bind + ngFor

--- aula-faculdade/src/app/app.component.ts
+++ aula-faculdade/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -27,25 +28,48 @@ export class AppComponent {
 // testando novamente o que aprendi na aula passada
   clientes = [
     {
+      id: 1,
       responsavel: 'antonio',
       empresa: 'Zé da Manga ltda.',
       cnpj: 758493759437
     },{
+      id: 2,
       responsavel: 'jubileu',
       empresa: 'Jujuba fresca me.',
       cnpj: 382747329422
     },{
+      id: 3,
       responsavel: 'MARCELO',
       empresa: 'Maracuja du bom mei.',
       cnpj: 84732974554
     },{
+      id: 4,
       responsavel: 'pamela',
       empresa: 'pamelas ltda.',
       cnpj: 75834594375
     }
   ]
 
-  clientePremium = true // TRUE aparecerá parágrafo
+  clientePremium: boolean = true // TRUE aparecerá parágrafo
                         // FALSE não aparecerá
+
+  // variáveis para novo cadastro
+  empresa: string = ""
+  cnpj!: number; // (!) diz ao Typescript que a varável será inicializada em algum momento antes de ser acessada.
+  responsavel: string = ""
+
+  adicionar() {
+    if (this.empresa === "" || this.cnpj === null || this.cnpj === undefined || this.responsavel === "")  {
+      window.alert('Please provide all data')
+    } else {
+      this.clientes.push({
+        // é necessário  preencher todas as chaves que houver
+        id: (this.clientes.length)+1,
+        responsavel: this.responsavel,
+        empresa: this.empresa,
+        cnpj: this.cnpj
+      })
+    }
+  }
 
 }
