2614bb61fd3c6f77d229586d128063143b4532c4
Guilherme <guilherme.teixeira013@protonmail.com>
2023-10-26 09:53:36 -0300

    comiits

--- conceitos-basicos/src/app/pages/binding-page.component.ts
+++ conceitos-basicos/src/app/pages/binding-page.component.ts
@@ -5,6 +5,7 @@ import { Component } from "@angular/core";
     selector: 'app-binding',
     template: ` 
     <h1>Imagem do Angular</h1>
+        <!-- Data binding one way -->
         <img [src]="imagem" [style]="{width: width}" />
     `
     }
